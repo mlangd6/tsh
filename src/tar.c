c5e878f8a791b5dcb4c3718997ea0321496f8766
mlangd <matthieu.langdorph@gmail.com>
2020-09-24 17:13:11 +0200

    Add main.c, tar.h and tar.c

---
+++ src/tar.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include "tar.h"
+
+/* Compute and write the checksum of a header, by adding all (unsigned) bytes in it (while hd->chksum is initially all ' '). Then hd->chksum is set to contain the octal encoding of this sum (on 6 bytes), followed by '\0' and ' '.
+   Code recupered for set_checksum(...) and check_checksum(...) on https://gaufre.informatique.univ-paris-diderot.fr/klimann/systL3_2020-2021/blob/master/TP/TP1/tar.h
+*/
+
+void set_checksum(struct posix_header *hd) {
+  memset(hd->chksum,' ',8);
+  unsigned int sum = 0;
+  char *p = (char *)hd;
+  for (int i=0; i < BLOCKSIZE; i++) { sum += p[i]; }
+  sprintf(hd->chksum,"%06o",sum);
+}
+
+/* Check that the checksum of a header is correct */
+
+int check_checksum(struct posix_header *hd) {
+  unsigned int checksum;
+  sscanf(hd->chksum,"%o ", &checksum);
+  unsigned int sum = 0;
+  char *p = (char *)hd;
+  for (int i=0; i < BLOCKSIZE; i++) { sum += p[i]; }
+  for (int i=0;i<8;i++) { sum += ' ' - hd->chksum[i]; }
+  return (checksum == sum);
+}
---
+++ src/tar.h
@@ -0,0 +1,39 @@
+/* tar Header Block, from POSIX 1003.1-1990.  */
+
+#define BLOCKSIZE 512
+#define BLOCKBITS 9
+
+/* POSIX header.  */
+
+/* Code recupered on https://www.gnu.org/software/tar/manual/html_node/Standard.html and on https://gaufre.informatique.univ-paris-diderot.fr/klimann/systL3_2020-2021/blob/master/TP/TP1/tar.h */
+
+struct posix_header
+{                              /* byte offset */
+  char name[100];               /*   0 */
+  char mode[8];                 /* 100 */
+  char uid[8];                  /* 108 */
+  char gid[8];                  /* 116 */
+  char size[12];                /* 124 */
+  char mtime[12];               /* 136 */
+  char chksum[8];               /* 148 */
+  char typeflag;                /* 156 */
+  char linkname[100];           /* 157 */
+  char magic[6];                /* 257 */
+  char version[2];              /* 263 */
+  char uname[32];               /* 265 */
+  char gname[32];               /* 297 */
+  char devmajor[8];             /* 329 */
+  char devminor[8];             /* 337 */
+  char prefix[155];             /* 345 */
+  char junk[12];                /* 500 */
+};                              /* Total: 512 */
+
+#define TMAGIC   "ustar"        /* ustar and a null */
+#define TMAGLEN  6
+#define TVERSION "00"           /* 00 and no null */
+#define TVERSLEN 2
+
+
+#define OLDGNU_MAGIC "ustar  "  /* 7 chars and a null */
+
+
