7a847f186bb759c7b6bba1957e00cf71d1aaa98f
mlangd <matthieu.langdorph@gmail.com>
2020-10-05 00:20:57 +0200

    Fix error in minunit.h

--- src/include/minunit.h
+++ src/include/minunit.h
@@ -1,9 +1,8 @@
 #ifndef MINUNIT_H
 #define MINUNIT_H
 /* Code taken from http://www.jera.com/techinfo/jtns/jtn002.html */
-extern int tests_run;
 #define mu_assert(message, test) do {  if (!(test)) return message; } while(0)
 #define m_run(test) do { char *message = test(); tests_run++; if (message) return message; } while(0)
-
+extern int tests_run;
 
 #endif
