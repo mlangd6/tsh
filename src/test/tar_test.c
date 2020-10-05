f6352c3c9f55cf064f2d4995718e668ca36db5af
mlangd <matthieu.langdorph@gmail.com>
2020-10-05 13:08:19 +0200

    Test tar_ls()

--- src/test/tar_test.c
+++ src/test/tar_test.c
@@ -12,13 +12,26 @@
 #define TAR_ADD_TEST_SIZE_BUF 700
 
 static char *tar_add_file_test();
+static char *test_tar_ls();
 int tests_run = 0;
 
-static char *(*tests[])(void) = {tar_add_file_test};
+
+static char *(*tests[])(void) = {tar_add_file_test, test_tar_ls};
 
 static char *tar_add_file_test() {
   return 0;
 }
+
+static char *test_tar_ls() {
+  before();
+  char **a_tester = tar_ls("/tmp/tsh_test/test.tar");
+  char *test[12] = {"dir1/", "dir1/subdir/", "dir1/subdir/subsubdir/", "dir1/subdir/subsubdir/hello", "dir1/tata", "man_dir/", "man_dir/man", "man_dir/open2", "man_dir/tar", "titi", "titi_link", "toto"};
+  for(int i = 0; i < 12; i++){
+    mu_assert("Error, this isn't the good ls", strcmp(test[i], a_tester[i]) == 0);
+  }
+  return 0;
+}
+
 static char *all_tests() {
   for (int i = 0; i < TAR_TEST_SIZE; i++) {
     before();
