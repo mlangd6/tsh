c201e8b061d437855c17be0aa27043f4a6247e1a
mlangd <matthieu.langdorph@gmail.com>
2020-10-14 00:39:41 +0200

    Implemantation auxiliaries functions

--- src/main/ls.c
+++ src/main/ls.c
@@ -8,8 +8,76 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <time.h>
 
 #define SIZE_OF_LINE 149
+
+static char *convert_rights_nb_in_ch(char *rights) {
+  char *tmp = malloc(10*sizeof(char));
+  int c = 1;
+  tmp[9] = '\0';
+  for(int i = 4; i < 6; i++, c++){
+    switch(rights[i]){
+      case '0' : tmp[c*(i%3)-1] = '-'; tmp[c*(i%3)] = '-'; tmp[c*(i%3)+1] = '-'; break;
+      case '1' : tmp[c*(i%3)-1] = '-'; tmp[c*(i%3)] = '-'; tmp[c*(i%3)+1] = 'x'; break;
+      case '2' : tmp[c*(i%3)-1] = '-'; tmp[c*(i%3)] = 'w'; tmp[c*(i%3)+1] = '-'; break;
+      case '3' : tmp[c*(i%3)-1] = '-'; tmp[c*(i%3)] = 'w'; tmp[c*(i%3)+1] = 'x'; break;
+      case '4' : tmp[c*(i%3)-1] = 'r'; tmp[c*(i%3)] = '-'; tmp[c*(i%3)+1] = '-'; break;
+      case '5' : tmp[c*(i%3)-1] = 'r'; tmp[c*(i%3)] = '-'; tmp[c*(i%3)+1] = 'x'; break;
+      case '6' : tmp[c*(i%3)-1] = 'r'; tmp[c*(i%3)] = 'w'; tmp[c*(i%3)+1] = '-'; break;
+      case '7' : tmp[c*(i%3)-1] = 'r'; tmp[c*(i%3)] = 'w'; tmp[c*(i%3)+1] = 'x'; break;
+    }
+  }
+  return tmp;
+}
+
+static char *is_directory(char c){
+  char *type = malloc(2*sizeof(char));
+  type[0] = '-';
+  type[1] = '\0';
+  if(c == DIRTYPE){
+    type[0] = 'd';
+  }
+  return type;
+}
+
+static char *convert_time(const char *ch){
+  struct tm *time = malloc(sizeof(struct tm));
+  time = getdate(ch);
+  char *tmp = malloc(100*sizeof(char));
+  sprintf(tmp, "%d", time->tm_mon);
+  return tmp;
+}
+
+static int color_directory(){
+    return -1;
+}
+
+static char *convert_size(char *size, int t){
+  int si = 0, cmp = 0, it = 0;
+  char *tmp = malloc(12*sizeof(char));
+  sscanf(size, "%o", &si);
+  for(int i = 0; i < t; i++){
+    if(it > 0){
+      tmp[cmp++] = size[i];
+      it = 1;
+    }
+    else if(size[i] != '0'){
+      tmp[cmp++] = size[i];
+      it = 1;
+    }
+    if(size[i] == '0' && it == 0 && size[i+1] != '\0'){
+      tmp[cmp++] = ' ';
+      it = 0;
+    }
+    else if(size[i] == '0' && it == 0 && size[i+1] == '\0'){
+      tmp[cmp++] = size[i];
+      it = 1;
+    }
+  }
+  tmp[cmp] = tmp[t-1];
+  return tmp;
+}
 
 static char *concat(char **all, int size)
 {
@@ -24,33 +92,14 @@ static char *concat(char **all, int size)
 
 static char **add_in_line(char **line, struct posix_header ph)
 {
-  line[0] = ph.mode;
+  printf("%s\n", convert_time(ph.mtime));
+  line[0] = strcat(is_directory(ph.typeflag), convert_rights_nb_in_ch(ph.mode));
   line[1] = ph.uid;
   line[2] = ph.gid;
-  line[3] = ph.size;
+  line[3] = convert_size(ph.size, 12);
   line[4] = ph.mtime;
   line[5] = ph.name;
   return line;
-}
-
-static char *convert_right_number_in_letter(char *rights) {
-  return NULL;
-}
-
-static char *is_directory(char *name){
-  return NULL;
-}
-
-static char *convert_time(char *time){
-  return NULL;
-}
-
-static int color_directory(){
-    return -1;
-}
-
-static char *convert_size(char *size){
-  return NULL;
 }
 
 static char *convert_name_user(char *user){
