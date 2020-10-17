57e46efe9bf5f33b0b58a3f458859ee65d3e8123
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2020-10-17 15:27:42 +0200

    Add reduce_path_abs() function which remove all the . and .. of a string

--- src/include/path_lib.h
+++ src/include/path_lib.h
@@ -5,4 +5,8 @@
     and returns the pointer of the next char */
 char *split_tar_abs_path(char const *path);
 
+/* Replace all the . and .. to get the real abs path of file
+  This function use malloc() and the return value should be free() */
+char *reduce_abs_path(char const *path);
+
 #endif
--- src/main/path_lib.c
+++ src/main/path_lib.c
@@ -25,3 +25,42 @@ char *split_tar_abs_path(char const *path) {
   free(cpy);
   return strchr(path, '\0');
 }
+
+/* Returns 0 if the first two char represents a "." dir,
+    1 if the first three char represents the parent ".." dir,
+    -1 else. */
+static int is_special_dir(char const *s) {
+  if (s[0] == '.') {
+    if (s[1] == '\0' || s[1] == '/') {
+      return 0;
+    }
+    if (s[1] == '.' && (s[2] == '/' || s[2] == '\0')) {
+      return 1;
+    }
+  }
+  return -1;
+}
+
+
+char *reduce_abs_path(char const *s) {
+  char *res = malloc(strlen(s) + 1); // Max size possible
+  strcpy(res, s);
+  char *prev_chr = res;
+  char *chr = res;
+  while ( (chr = strchr(chr, '/')) != NULL) {
+    switch (is_special_dir(++chr)) {
+      case 0:
+        strcpy(chr, chr + 2);
+        break;
+      case 1:
+        if (chr - 1 == res) { // path starts by /..
+          return NULL;
+        }
+        else {
+          strcpy(prev_chr, chr + 3);
+        }
+    }
+    prev_chr = chr;
+  }
+  return res;
+}
