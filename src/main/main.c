c8998681cf78165769a48f5286ec8344eac6a793
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2020-10-20 01:21:17 +0200

    Fix commands that doesn't involve tar

--- src/main/main.c
+++ src/main/main.c
@@ -14,6 +14,11 @@
 #define CMD_NOT_FOUND_SIZE 22
 #define NB_TAR_CMD 1
 
+static int count_words(char *s);
+static char **split(char *s, int *is_tar_cmd);
+static void init_tsh();
+static int is_tar_command(char *s);
+
 char tsh_dir[PATH_MAX];
 char *tar_cmds[NB_TAR_CMD] = {"cat"};
 char act_path[PATH_MAX];
@@ -27,32 +32,35 @@ static int count_words(char *s) {
   return res;
 }
 
-static char **split(char *s) {
+static char **split(char *s, int *is_tar_cmd) {
   int nb_tokens = count_words(s);
   char delim[] = " ";
   char **res = malloc((nb_tokens+1) * sizeof(char *));
   res[0] = strtok(s, delim);
+  *is_tar_cmd = is_tar_command(res[0]);
   for (int i = 1; i < nb_tokens; i++) {
     res[i] = strtok(NULL, delim);
-    if (res[i][0] != '-') { // Not an option
-      if (res[i][0] != '/') { // Relative path
-        char *tmp = malloc(PATH_MAX);
-        strcpy(tmp, act_path);
-        strcat(tmp, "/");
-        strcat(tmp, res[i]);
-        res[i] = tmp;
+    if (*is_tar_cmd) {
+      if (res[i][0] != '-') { // Not an option
+        if (res[i][0] != '/') { // Relative path
+          char *tmp = malloc(PATH_MAX);
+          strcpy(tmp, act_path);
+          strcat(tmp, "/");
+          strcat(tmp, res[i]);
+          res[i] = tmp;
+        }
+        else { // Absolute path
+          char *tmp = malloc(PATH_MAX);
+          strcpy(tmp, res[i]);
+          res[i] = tmp;
+        }
+        reduce_abs_path(res[i]);
       }
-      else { // Absolute path
-        char *tmp = malloc(PATH_MAX);
+      else {
+        char *tmp = malloc(sizeof(res[i]) + 1);
         strcpy(tmp, res[i]);
         res[i] = tmp;
       }
-      reduce_abs_path(res[i]);
-    }
-    else {
-      char *tmp = malloc(sizeof(res[i]) + 1);
-      strcpy(tmp, res[i]);
-      res[i] = tmp;
     }
   }
   res[nb_tokens] = NULL;
@@ -78,18 +86,19 @@ static int is_tar_command(char *s) {
 int main(int argc, char *argv[]){
   init_tsh();
   char *buf;
+  int is_tar_cmd;
   while((buf = readline(PROMPT))) {
     if (strlen(buf) > 0) {
       add_history(buf);
     }
-    char **tokens = split(buf);
+    char **tokens = split(buf, &is_tar_cmd);
     int p = fork(), w;
     switch (p) {
       case -1:
         perror("fork");
         exit(EXIT_FAILURE);
       case 0: //son
-        if (is_tar_command(tokens[0])) {
+        if (is_tar_cmd) {
           char cmd_exec[PATH_MAX];
           strcpy(cmd_exec, tsh_dir);
           strcat(cmd_exec, "/bin/");
@@ -111,10 +120,13 @@ int main(int argc, char *argv[]){
         wait(&w);
 
     }
-    for (int i = 0; tokens[i] != NULL; i++) {
-      free(tokens[i]);
+    if (is_tar_cmd) {
+      for (int i = 1; tokens[i] != NULL; i++) {
+        free(tokens[i]);
+      }
     }
-
+    free(tokens);
+    free(buf);
   }
   return 0;
 }
