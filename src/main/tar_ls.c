a0f2e5f60c61894d872ea2f18a7989cb5a5d1692
BOUARAH Romain <romain.bouarah@etu.univ-paris-diderot.fr>
2020-10-23 19:21:16 +0200

    Refactor src/main/tar_ls.c

--- src/main/tar_ls.c
+++ src/main/tar_ls.c
@@ -1,59 +1,63 @@
 #include <assert.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include "errors.h"
 #include "tar.h"
 
 
-/* count the number of file in a file .tar */
+/* Count the number of file in the tar referenced by TAR_FD. */
 static int nb_file_in_tar(int tar_fd)
 {
-  int i = 0;
-  int n;
+  ssize_t read_size;
+  int nb = 0;
   struct posix_header header;
 
-  while ( (n = read(tar_fd, &header, BLOCKSIZE)) > 0 )
+  while ((read_size = read(tar_fd, &header, BLOCKSIZE)) > 0 )
     {
-      if (strcmp(header.name, "\0") == 0)
+      if(read_size != BLOCKSIZE)
+	return -1;
+      if(header.name[0] == '\0')
 	break;
       else
-	i++;
+	nb++;
       
       skip_file_content(tar_fd, &header);
     }
 
   lseek(tar_fd, 0, SEEK_SET);
-  return i;
+  return nb;
 }
 
 
+/* Return an array of all files header of a tar located at path TAR_NAME. */
 struct posix_header *tar_ls(const char *tar_name)
 {
   int tar_fd = open(tar_name, O_RDONLY);
-  if (tar_fd == -1)
-    {
-      return error_p(tar_name, &tar_fd, 1);
-    }
-  int n;
-  int i = 0;
-  struct posix_header header;
-  struct posix_header *list_header = malloc(nb_file_in_tar(tar_fd)*sizeof(struct posix_header));
+  if (tar_fd < 0)
+    return error_p(tar_name, &tar_fd, 1);
+
+  int nb_file = nb_file_in_tar(tar_fd);
+  if(nb_file < 0)
+    return error_p(tar_name, &tar_fd, 1);
+  
+  ssize_t read_size;
+
+  struct posix_header *list_header = malloc(nb_file * sizeof(struct posix_header));
   assert(list_header);
 
-  while ( (n = read(tar_fd, &header, BLOCKSIZE)) > 0 )
+  for(int i=0; i < nb_file; i++)
     {
-      if (strcmp(header.name, "\0") == 0)
-	break;
+      read_size = read(tar_fd, list_header+i, BLOCKSIZE);
 
-      list_header[i++] = header;
-
-      skip_file_content(tar_fd, &header);
+      if(read_size != BLOCKSIZE)
+	{
+	  free(list_header);
+	  return error_p(tar_name, &tar_fd, 1);
+	}
+      
+      skip_file_content(tar_fd, list_header+i);
     }
   
   close(tar_fd);
