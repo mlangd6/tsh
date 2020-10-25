5663ccde882dce2d9f243ebf3b99c3837cc2822d
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2020-10-25 15:17:12 +0100

    Add write_error function to concatenate and write message on stderr

--- src/include/errors.h
+++ src/include/errors.h
@@ -7,4 +7,7 @@ void *error_p(const char *, int[], int);
 /* Manage error caused by a file, and close all the files opened, and return an integer. */
 int error_pt(const char *, int[], int);
 
+/* Write error message to STDERR_FILENO in the order of arguments and returns EXIT_FAILURE */
+int write_error(const char *pre_error, const char *arg, const char *post_error);
+
 #endif
--- src/main/errors.c
+++ src/main/errors.c
@@ -1,4 +1,6 @@
 #include "errors.h"
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -20,4 +22,13 @@ int error_pt(const char *s, int fds[], int length_fds)
     close(fds[i]);
   }
   return -1;
+}
+
+int write_error(const char *pre_error, const char *arg, const char *post_error) {
+  char *error = malloc(strlen(pre_error) + strlen(arg) + strlen(post_error) + 1);
+  memcpy(error, pre_error, strlen(pre_error));
+  memcpy(error, arg, strlen(arg));
+  memcpy(error, post_error, strlen(post_error) + 1);
+  write(STDERR_FILENO, error, strlen(error) + 1);
+  return EXIT_FAILURE;
 }
