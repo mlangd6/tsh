1cb85b1072197c334960dd6a7d4f216900e1c8de
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2020-11-06 12:03:11 +0100

    Add type_of_user function

--- src/main/tar_access.c
+++ src/main/tar_access.c
@@ -6,10 +6,30 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <pwd.h>
 
 #include "errors.h"
 #include "tar.h"
 #include "utils.h"
+
+
+/* Get the type of user according to HD,
+   Returns:
+   0 if current user is the user in hd
+   1 if current groupe is the same of hd
+   2 else
+*/
+static int type_of_user(struct posix_header *hd)
+{
+  struct passwd *pwd = getpwuid(getuid());
+  int uid = strtol(hd -> uid, NULL, 8);
+  if (uid == pwd -> pw_uid)
+    return 0;
+  int gid =strtol(hd -> gid, NULL, 8);
+  if (gid == pwd -> pw_uid)
+    return 1;
+  return 2;
+}
 
 static int is_mode_correct(int mode)
 {
@@ -25,14 +45,14 @@ int tar_access(const char *tar_name, const char *file_name, int mode)
       errno = EINVAL;
       return -1;
     }
-  
+
   int tar_fd = open(tar_name, O_RDONLY);
   if(tar_fd < 0)
     return -1;
   size_t nb_headers = nb_files_in_tar(tar_fd);
   close(tar_fd);
 
-  
+
   struct posix_header *headers = tar_ls(tar_name);
   if( !headers )
     return -1;
@@ -45,11 +65,11 @@ int tar_access(const char *tar_name, const char *file_name, int mode)
       if(!strcmp(headers[i].name, file_name)) // fichier exactement trouvé
 	found = 1;
       else if(is_dir && is_prefix(file_name, headers[i].name)) // dossier existant à travers ses sous-fichiers
-	found = 2;      
+	found = 2;
     }
 
   free(headers);
-  
+
   if(!found)
     {
       errno = ENOENT;
@@ -58,4 +78,3 @@ int tar_access(const char *tar_name, const char *file_name, int mode)
 
   return found;
 }
-
