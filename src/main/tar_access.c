bf48e13e99aa326ed174a423fee28c772ce543d7
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2020-11-07 10:32:52 +0100

    Add tests of type of user when belonging to multiple groups in tar_access

--- src/main/tar_access.c
+++ src/main/tar_access.c
@@ -7,6 +7,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <pwd.h>
+#include <grp.h>
+#include <linux/limits.h>
 
 #include "errors.h"
 #include "tar.h"
@@ -24,27 +26,39 @@ static int is_mode_correct(int mode)
    1 if current groupe is the same of hd
    2 else
 */
-static int type_of_user(struct posix_header hd)
+static int type_of_user(struct posix_header hd, struct passwd *pwd, gid_t *groups, int nb_groups)
 {
-  struct passwd *pwd = getpwuid(getuid());
-  int uid = strtol(hd.uid, NULL, 8);
+
+  uid_t uid = strtol(hd.uid, NULL, 8);
   if (uid == pwd -> pw_uid)
     return 0;
-  int gid =strtol(hd.gid, NULL, 8);
-  if (gid == pwd -> pw_uid)
+  gid_t gid =strtol(hd.gid, NULL, 8);
+  for (int i = 0; i < nb_groups; i++)
+  {
+    if (gid == groups[i])
+      return 1;
+  }
+  /* Test current gid because: it is unspecified whether the effective group ID
+     of the calling process is included in the returned list (getgroups man page)*/
+  if (pwd -> pw_gid == groups[1])
     return 1;
   return 2;
 }
 /* Returns 0 if current user has the rights that are in MODE in the header HD */
-static int has_rights(struct posix_header hd, int mode)
+static int has_rights(struct posix_header hd, struct passwd *pwd, int mode)
 {
-  int type_u = type_of_user(hd);
+  int nb_groups = getgroups(0, NULL);
+  gid_t *groups = malloc(nb_groups * sizeof(gid_t));
+  getgroups(nb_groups, groups);
+
+  int type_u = type_of_user(hd, pwd, groups, nb_groups);
   int rights[] =
   {
     hd.mode[4] - '0',
     hd.mode[5] - '0',
     hd.mode[6] - '0'
   };
+
   if ( (mode & R_OK && !(4 & rights[type_u]))
   ||   (mode & W_OK && !(2 & rights[type_u]))
   ||   (mode & X_OK && !(1 & rights[type_u])) )
@@ -60,7 +74,7 @@ static int has_rights(struct posix_header hd, int mode)
    1 if file was found and has the rights
    2 if file is a dir and has beeen found in his subfile
 */
-static int simple_tar_access(const char *filename, struct posix_header *hds, int nb_hds, int mode)
+static int simple_tar_access(const char *filename, struct posix_header *hds, int nb_hds, struct passwd *pwd, int mode)
 {
   int found = 0;
   int index = -1;
@@ -83,12 +97,12 @@ static int simple_tar_access(const char *filename, struct posix_header *hds, int
   else if (mode == F_OK || found == 2)
     return found;
   // else found == 1
-  return (has_rights(hds[index], mode) == 0) ? 1 : -1;
+  return (has_rights(hds[index], pwd, mode) == 0) ? 1 : -1;
 
 }
 
 /* Check user's permissions for every parent directory of FILENAME and FILENAME itself */
-static int tar_access_all(const char *filename, struct posix_header *hds, int nb_hds, int mode)
+static int tar_access_all(const char *filename, struct posix_header *hds, int nb_hds, struct passwd *pwd, int mode)
 {
   size_t filename_len = strlen(filename);
   char *cpy = malloc(filename_len + 1);
@@ -99,7 +113,7 @@ static int tar_access_all(const char *filename, struct posix_header *hds, int nb
   {
     tmp = it[1];
     it[1] = '\0';
-    if (simple_tar_access(cpy, hds, nb_hds, mode) == -1)
+    if (simple_tar_access(cpy, hds, nb_hds, pwd, mode) == -1)
     {
       it[1] = tmp;
       free(cpy);
@@ -107,7 +121,7 @@ static int tar_access_all(const char *filename, struct posix_header *hds, int nb
     }
     it[1] = tmp;
   }
-  return simple_tar_access(cpy, hds, nb_hds, mode);
+  return simple_tar_access(cpy, hds, nb_hds, pwd, mode);
 }
 
 /* Check user's permissions for file FILE_NAME in tar at path TAR_NAME */
