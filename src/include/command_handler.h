3d6080539e92dd3c3f3f0230b22883729e97aced
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2020-11-15 15:46:42 +0100

    Manage print of arg if multiple arguments

--- src/include/command_handler.h
+++ src/include/command_handler.h
@@ -4,15 +4,23 @@
 typedef struct command {
   char *name;
   int (*in_tar_func) (char *, char *, char *);
-  int twd_arg;
+  short twd_arg;
+  short print_multiple_arg;
   char *support_opt;
 } command;
+
+typedef struct arg_info {
+  int nb_in_tar;
+  int nb_out;
+  int opt_c;
+  char **options;
+} arg_info ;
 
 int handle(command c, int argc, char **argv);
 
 /* Return 1 if at least one argument of ARGV implies a tarball
    2 if there is no argument (other than options)
    0 else */
-int has_tar_arg(int argc, char **argv);
+void parse_args(int argc, char **argv, arg_info *info);
 
 #endif
--- src/main/command_handler.c
+++ src/main/command_handler.c
@@ -10,18 +10,22 @@
 #include "tar.h"
 #include "command_handler.h"
 
-static int tar_arg_and_options(int argc, char **argv, int *opt_c, char **options);
 
 int handle(command cmd, int argc, char **argv) {
-  int opt_c;
-  char **options = calloc(argc, sizeof(char *));
-  int tar_arg = tar_arg_and_options(argc, argv, &opt_c, options);
-  if (!tar_arg || (tar_arg == 2 && !cmd.twd_arg))
+  arg_info info = {
+    0,
+    0,
+    0,
+    calloc(argc, sizeof(char *))
+  };
+  parse_args(argc, argv, &info);
+  if (info.nb_in_tar == 0 && (info.nb_out > 0 || !cmd.twd_arg))
     execvp(cmd.name, argv);
   int c;
   opterr = 0;
   int ret = EXIT_SUCCESS;
   int not_tar_opt = 0;
+  int opt_c = 0;
   int sup_opt_len = strlen(cmd.support_opt);
   char *tar_opt = malloc(sup_opt_len  + 1);
   memset(tar_opt, '\0', sup_opt_len + 1);
@@ -41,31 +45,50 @@ int handle(command cmd, int argc, char **argv) {
     else if (strchr(tar_opt, c) == NULL)
       tar_opt[tar_opt_c++] = c;
   }
-  if (tar_arg == 2) // No arguments (cmd.twd_arg == 1)
+  if (info.nb_in_tar == 0 && info.nb_out == 0) // No arguments (cmd.twd_arg == 1)
   {
     char *tmp = getenv("PWD");
     char twd[PATH_MAX];
     memcpy(twd, tmp, strlen(tmp) + 1);
     char *in_tar = split_tar_abs_path(twd);
     if (*in_tar != '\0' || is_tar(twd) == 1)
+    {
       return (not_tar_opt) ? EXIT_FAILURE : cmd.in_tar_func(twd, in_tar, tar_opt);
-
+    }
+    else execvp(cmd.name, argv);
   }
   // tar_arg == 1
+  int rest = info.nb_out + ((not_tar_opt) ? 0: info.nb_in_tar);
+  int nb_valid_arg = rest;
   char *in_tar;
   for (int i = 1; i < argc; i++)
   {
     if (*argv[i] == '-')
       continue;
+    char cpy[PATH_MAX];
+    memcpy(cpy, argv[i], strlen(argv[i]) + 1);
     in_tar = split_tar_abs_path(argv[i]);
     if (*in_tar != '\0' || is_tar(argv[i]) == 1)
     {
-      if (!not_tar_opt && cmd.in_tar_func(argv[i], in_tar, tar_opt) == EXIT_FAILURE)
-        ret = EXIT_FAILURE;
+      if (!not_tar_opt) {
+        if (cmd.print_multiple_arg && nb_valid_arg > 1)
+        {
+          write(STDOUT_FILENO, cpy, strlen(cpy));
+          write(STDOUT_FILENO, ": \n", 3);
+        }
+        cmd.in_tar_func(argv[i], in_tar, tar_opt);
+        if (--rest > 0 && cmd.print_multiple_arg)
+          write(STDOUT_FILENO, "\n", 1);
+      }
     }
     else
     {
-      options[opt_c] = argv[i];
+      if (cmd.print_multiple_arg && nb_valid_arg > 1)
+      {
+        write(STDOUT_FILENO, argv[i], strlen(argv[i]));
+        write(STDOUT_FILENO, ":\n", 2);
+      }
+      info.options[opt_c] = argv[i];
       int status, p = fork();
       switch (p)
       {
@@ -73,37 +96,37 @@ int handle(command cmd, int argc, char **argv) {
           error_cmd(cmd.name, "fork");
           break;
         case 0: // child
-          execvp(cmd.name, options);
+          execvp(cmd.name, info.options);
           break;
         default: // parent
           wait(&status);
           if (WEXITSTATUS(status) != EXIT_SUCCESS)
             ret = EXIT_FAILURE;
       }
-      options[opt_c] = NULL;
+      info.options[opt_c] = NULL;
+      if (--rest > 0 && cmd.print_multiple_arg)
+      write(STDOUT_FILENO, "\n", 1);
     }
   }
-  free(options);
+  free(info.options);
   return ret;
 }
 
-static int tar_arg_and_options(int argc, char **argv, int *opt_c, char **options)
+void parse_args(int argc, char **argv, arg_info *info)
 {
-  int ret = 2;
   char cpy[PATH_MAX];
   for (size_t len, i = 1; i < argc; i++)
   {
     len = strlen(argv[i]) + 1;
-    if (*argv[i] == '-' && ++(*opt_c))
-      options[*(opt_c)-1] = argv[i];
-    else if (ret != 1)
+    if (*argv[i] == '-' && ++(info -> opt_c))
+      info -> options[info -> opt_c-1] = argv[i];
+    else
     {
       memmove(cpy, argv[i], len);
       char *in_tar = split_tar_abs_path(cpy);
       if ( is_tar(cpy) == 1 || *in_tar != '\0')
-        ret = 1;
-      else ret = 0;
+        info -> nb_in_tar++;
+      else info -> nb_out++;
     }
   }
-  return ret;
 }
