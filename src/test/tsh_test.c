9f0016b2d1d580b8493217de59de78bc2f07c627
BOUARAH Romain <romain.bouarah@etu.univ-paris-diderot.fr>
2020-11-23 17:56:07 +0100

    Add test for stack data structure

--- src/test/tsh_test.c
+++ src/test/tsh_test.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "list_test.h"
 #include "minunit.h"
-#include "path_lib_test.h"
-#include "tar_test.h"
 #include "tsh_test.h"
 
+#include "path_lib_test.h"
+#include "tar_test.h"
+#include "list_test.h"
+#include "stack_test.h"
 
 int tests_run;
 
@@ -22,6 +23,7 @@ int main(int argc, char const *argv[])
   ret = launch_path_lib_tests() && ret;
   ret = launch_tar_tests() && ret;
   ret = launch_list_tests() && ret;
+  ret = launch_stack_tests() && ret;
   
   if (ret)
     printf("ALL TESTS PASSED\n");
---
+++ src/test_include/stack_test.h
@@ -0,0 +1,7 @@
+#ifndef STACK_TEST_H
+#define STACK_TEST_H
+
+int launch_stack_tests();
+
+#endif
+
