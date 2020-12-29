ec8f6d2269f65c1dfbe06b078a7a9d5749b2b5e3
SOUFFAN Nathan <nathan.souffan@etu.univ-paris-diderot.fr>
2020-12-29 19:16:01 +0100

    Remove '/' at the end of pwd

--- src/main/tsh.c
+++ src/main/tsh.c
@@ -9,6 +9,7 @@
 #include "errors.h"
 #include "tar.h"
 #include "tsh.h"
+#include "utils.h"
 
 static int ret_value;
 static int cd(char **argv, int argc);
@@ -100,7 +101,7 @@ static int cd(char **argv, int argc)
     if (home == NULL)
     {
       error (0, "tsh: cd: HOME not set\n");
-      return EXIT_FAILURE;      
+      return EXIT_FAILURE;
     }
 
     if (chdir(home) != 0)
@@ -119,7 +120,7 @@ static int cd(char **argv, int argc)
       if (argv[1][1] != '\0') // Not supported option
       {
 	error (0, "tsh: cd: - est la seule option supportée\n");
-        return EXIT_FAILURE;        
+        return EXIT_FAILURE;
       }
       else // Supported option
       {
@@ -173,7 +174,7 @@ static int cd(char **argv, int argc)
           error_cmd("tsh: cd", argv[1]);
           return EXIT_FAILURE;
         }
-
+        if (strcmp(path, "/") != 0) remove_last_slash(path);
         setenv("OLDPWD", pwd, 1);
         setenv("PWD", path, 1);
       }
@@ -183,9 +184,9 @@ static int cd(char **argv, int argc)
         {
           if (tar_access(path, in_tar, X_OK) != -1) // check execution permission
           {
-            int in_tar_len = strlen(in_tar);
-            in_tar[in_tar_len-1] = '\0';
-            in_tar[-1] = '/';
+            remove_last_slash(in_tar);
+            char buf[PATH_MAX + 1];
+            sprintf(buf, "%s/%s", path, in_tar);
             setenv("OLDPWD", pwd, 1);
             setenv("PWD", path, 1);
             in_tar[-1] = '\0';
